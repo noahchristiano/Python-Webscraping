814e41e7d492775d160fbcb9069b9ff434d101c5
Noah Christiano <noahchristiano@outlook.com>
2015-03-10 04:53:11 -0400

    First commit

---
+++ Lowes/locations/locations/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+# Lowes
+# Created by Noah Christiano on 7/21/2014.
+# noahchristiano@rochester.edu
---
+++ Rap Genius/genius/genius/spiders/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+# Rap Genius
+# Created by Noah Christiano on 8/15/2014.
+# noahchristiano@rochester.edu
+
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
